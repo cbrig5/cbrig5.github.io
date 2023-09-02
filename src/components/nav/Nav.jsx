c08e9e9f7c9729fb04af2d65b8efadf7e874df63
Corey Bright <95330156+cbright3636@users.noreply.github.com>
2023-09-02 02:00:51 -0400

    [IMP] react-portfolio: Improved the nav bar by dynamically showing Nav States through useState Hooks

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,24 +5,45 @@ import { AiOutlineUser } from "react-icons/ai";
 import { MdWorkOutline } from "react-icons/md";
 import { AiOutlineProject } from "react-icons/ai";
 import { BiSolidContact } from "react-icons/bi";
-import {useState} from 'react';
+import { useState } from "react";
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState("#");
   return (
     <nav>
-      <a href="#" className="active">
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
         <BiHome />
       </a>
-      <a href="#about">
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
         <AiOutlineUser />
       </a>
-      <a href="#experience">
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
         <MdWorkOutline />
       </a>
-      <a href="#portfolio">
+      <a
+        href="#portfolio"
+        onClick={() => setActiveNav("#portfolio")}
+        className={activeNav === "#portfolio" ? "active" : ""}
+      >
         <AiOutlineProject />
       </a>
-      <a href="#contact">
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
         <BiSolidContact />
       </a>
     </nav>
