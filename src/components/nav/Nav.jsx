68ace136abba08d17967d53e60469818aa5d8757
Corey Bright <95330156+cbright3636@users.noreply.github.com>
2023-09-02 01:49:32 -0400

    [ADD] react-portfolio: Adds a functioning nav bar to the bottom of the website

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import './nav.css'
+import React from "react";
+import "./nav.css";
+import { BiHome } from "react-icons/bi";
+import { AiOutlineUser } from "react-icons/ai";
+import { MdWorkOutline } from "react-icons/md";
+import { AiOutlineProject } from "react-icons/ai";
+import { BiSolidContact } from "react-icons/bi";
+import {useState} from 'react';
 
 const Nav = () => {
   return (
-    <div>Nav</div>
-  )
-}
+    <nav>
+      <a href="#" className="active">
+        <BiHome />
+      </a>
+      <a href="#about">
+        <AiOutlineUser />
+      </a>
+      <a href="#experience">
+        <MdWorkOutline />
+      </a>
+      <a href="#portfolio">
+        <AiOutlineProject />
+      </a>
+      <a href="#contact">
+        <BiSolidContact />
+      </a>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
