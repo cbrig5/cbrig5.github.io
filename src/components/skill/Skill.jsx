b82979cf3001cb7e5081da9318ce700323c693ff
Corey Bright <95330156+cbright3636@users.noreply.github.com>
2023-09-05 23:14:41 -0400

    [ADD] react-portfolio: Adds html and css to skill(previous experience) section.
    The experience sections name was changed to skill.

---
+++ src/components/skill/Skill.jsx
@@ -0,0 +1,122 @@
+import React from "react";
+import "./skill.css";
+import { PiSealCheckFill } from "react-icons/pi";
+
+const Skill = () => {
+  return (
+    <section id="Skill">
+      <h5>Exploring my abilities</h5>
+      <h2>My Skills</h2>
+      <div className="container skill__container">
+        <div className="skill__frontend">
+          <h3>Frontend Development</h3>
+          <div className="skill__content">
+            <article className="skill__details">
+              <PiSealCheckFill className="skill__details-icon" />
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Proficient</small>
+              </div>
+            </article>
+
+            <article className="skill__details">
+              <PiSealCheckFill className="skill__details-icon" />
+
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light">Proficient</small>
+              </div>
+            </article>
+
+            <article className="skill__details">
+              <PiSealCheckFill className="skill__details-icon" />
+              <div>
+                <h4>Javascript</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+
+            <article className="skill__details">
+              <PiSealCheckFill className="skill__details-icon" />
+              <div>
+                <h4>Responsive Web Design</h4>
+                <small className="text-light">Proficient</small>
+              </div>
+            </article>
+
+            <article className="skill__details">
+              <PiSealCheckFill className="skill__details-icon" />
+              <div>
+                <h4>React</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+
+            <article className="skill__details">
+              <PiSealCheckFill className="skill__details-icon" />
+              <div>
+                <h4>Full-stack Coordination</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
+
+        <div className="skill__backend">
+          <h3>Backend Development</h3>
+          <div className="skill__content">
+            <article className="skill__details">
+              <PiSealCheckFill className="skill__details-icon" />
+              <div>
+                <h4>Python</h4>
+                <small className="text-light">Proficient</small>
+              </div>
+            </article>
+
+            <article className="skill__details">
+              <PiSealCheckFill className="skill__details-icon" />
+              <div>
+                <h4>Node.Js</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+
+            <article className="skill__details">
+              <PiSealCheckFill className="skill__details-icon" />
+              <div>
+                <h4>MySQL</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+
+            <article className="skill__details">
+              <PiSealCheckFill className="skill__details-icon" />
+              <div>
+                <h4>PostgreSQL</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+
+            <article className="skill__details">
+              <PiSealCheckFill className="skill__details-icon" />
+              <div>
+                <h4>Java</h4>
+                <small className="text-light">Proficient</small>
+              </div>
+            </article>
+
+            <article className="skill__details">
+              <PiSealCheckFill className="skill__details-icon" />
+              <div>
+                <h4>Version Control</h4>
+                <small className="text-light">Proficient</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Skill;
